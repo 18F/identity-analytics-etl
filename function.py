fa1e22adc6d3e015c9ee0f4ad49657dd63a65934
Jordan Bramble <jordan.k.bramble@omb.eop.gov>
2017-07-12 15:34:01 -0400

    removes port from redshift host, as lambda provides that as env variable

--- function.py
+++ function.py
@@ -15,7 +15,7 @@ def set_redshift_configs():
     # could all be defined inside of the lambda resource in terraform.
     bucket = boto3.resource('s3').Bucket("login-gov-{}-redshift-secrets".format(os.environ.get('env')))
     data = yaml.load(bucket.Object('redshift_secrets.yml').get()['Body'])
-    os.environ['REDSHIFT_URI'] = "redshift+psycopg2://awsuser:{redshift_password}@{redshift_host}:5439/analytics".format(
+    os.environ['REDSHIFT_URI'] = "redshift+psycopg2://awsuser:{redshift_password}@{redshift_host}/analytics".format(
         redshift_password=data['redshift_password'],
         redshift_host=os.environ.get('redshift_host')
     )
