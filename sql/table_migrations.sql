d090a69712e78c86c5861fd691e860bef6b68c6b
Jordan Bramble <jordan.k.bramble@omb.eop.gov>
2017-11-09 11:21:15 -0500

    adds begin and end

--- sql/table_migrations.sql
+++ sql/table_migrations.sql
@@ -1,50 +1,54 @@
-CREATE TABLE events_tt (
-  id VARCHAR(40) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  user_agent VARCHAR(1024),
-  user_id VARCHAR(40),
-  user_ip VARCHAR(50),
-  host VARCHAR(255),
-  visit_id VARCHAR(40),
-  visitor_id VARCHAR(40),
-  time TIMESTAMP,
-  event_properties VARCHAR(4096),
+BEGIN;
+  CREATE TABLE events_tt (
+    id VARCHAR(40) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    user_agent VARCHAR(1024),
+    user_id VARCHAR(40),
+    user_ip VARCHAR(50),
+    host VARCHAR(255),
+    visit_id VARCHAR(40),
+    visitor_id VARCHAR(40),
+    time TIMESTAMP,
+    event_properties VARCHAR(4096),
 
-  success BOOLEAN,
-  existing_user BOOLEAN,
-  otp_method VARCHAR(20),
-  context VARCHAR(20),
-  method VARCHAR(20),
-  authn_context VARCHAR(50),
-  service_provider VARCHAR(255),
-  loa3 BOOLEAN,
-  active_profile BOOLEAN,
-  errors VARCHAR(4096))
-  DISTKEY(time), SORTKEY(time);
+    success BOOLEAN,
+    existing_user BOOLEAN,
+    otp_method VARCHAR(20),
+    context VARCHAR(20),
+    method VARCHAR(20),
+    authn_context VARCHAR(50),
+    service_provider VARCHAR(255),
+    loa3 BOOLEAN,
+    active_profile BOOLEAN,
+    errors VARCHAR(4096))
+    DISTKEY(time), SORTKEY(time);
 
-SELECT * INTO events_tt FROM events;
+  SELECT * INTO events_tt FROM events;
 
-DROP TABLE events;
-ALTER TABLE events_tt RENAME TO events;
+  DROP TABLE events;
+  ALTER TABLE events_tt RENAME TO events;
+END;
 
-CREATE TABLE pageviews_tt (
-  method VARCHAR(10) NOT NULL,
-  path VARCHAR(1024),
-  format VARCHAR(255),
-  controller VARCHAR(100),
-  action VARCHAR(15),
-  status SMALLINT,
-  duration FLOAT,
-  user_id VARCHAR(40),
-  user_agent VARCHAR(1024),
-  ip VARCHAR(50),
-  host VARCHAR(255),
-  timestamp TIMESTAMP,
-  uuid VARCHAR(64) NOT NULL
-  )
-  DISTKEY(timestamp), SORTKEY(timestamp);
+BEGIN;
+  CREATE TABLE pageviews_tt (
+    method VARCHAR(10) NOT NULL,
+    path VARCHAR(1024),
+    format VARCHAR(255),
+    controller VARCHAR(100),
+    action VARCHAR(15),
+    status SMALLINT,
+    duration FLOAT,
+    user_id VARCHAR(40),
+    user_agent VARCHAR(1024),
+    ip VARCHAR(50),
+    host VARCHAR(255),
+    timestamp TIMESTAMP,
+    uuid VARCHAR(64) NOT NULL
+    )
+    DISTKEY(timestamp), SORTKEY(timestamp);
 
-  SELECT * INTO pageviews_tt FROM pageviews;
+    SELECT * INTO pageviews_tt FROM pageviews;
 
-  DROP TABLE pageviews;
-  ALTER TABLE pageviews_tt RENAME TO pageviews;
+    DROP TABLE pageviews;
+    ALTER TABLE pageviews_tt RENAME TO pageviews;
+END;
