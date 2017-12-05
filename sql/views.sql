c7990e2125e9e3257caa28dfab0087524c57a0d2
Jordan Bramble <Jordan.K.Bramble@omb.eop.gov>
2017-12-05 15:42:08 +0000

    this fixes a syntax error in views.sql (#42)

--- sql/views.sql
+++ sql/views.sql
@@ -18,91 +18,91 @@ as (
 
 CREATE VIEW experience_durations AS (
   WITH tj as (
-    SELECT 
-      t1.name as t1_name, 
-      t2.name as t2_name, 
-      t1.user_id as t1_user_id, 
-      t1.visit_id as t1_visit_id, 
+    SELECT
+      t1.name as t1_name,
+      t2.name as t2_name,
+      t1.user_id as t1_user_id,
+      t1.visit_id as t1_visit_id,
       EXTRACT(epoch FROM (t2.time - t1.time)) AS duration,
-      t1.time as time 
-    FROM events t1 
-    JOIN events t2 
-    ON 
-      t1.user_id = t2.user_id 
-    AND 
+      t1.time as time
+    FROM events t1
+    JOIN events t2
+    ON
+      t1.user_id = t2.user_id
+    AND
       t1.visit_id = t2.visit_id
     WHERE EXTRACT(epoch FROM (t2.time - t1.time)) > 0
   )
   SELECT q.exp_name,q.duration,q.time FROM
   (
-      SELECT 
-        'email submission' AS exp_name, 
-        duration, 
+      SELECT
+        'email submission' AS exp_name,
+        duration,
         tj.time as time
-      FROM tj 
-      WHERE tj.t1_name = 'User Registration: enter email visited' 
-      AND tj.t2_name = 'User Registration: Email Submitted' 
+      FROM tj
+      WHERE tj.t1_name = 'User Registration: enter email visited'
+      AND tj.t2_name = 'User Registration: Email Submitted'
     UNION
-      SELECT 
-        'phone setup' AS exp_name, 
-        duration, 
+      SELECT
+        'phone setup' AS exp_name,
+        duration,
         tj.time as time
-      FROM tj 
-      WHERE tj.t1_name = 'User Registration: phone setup visited' 
-      AND tj.t2_name = 'User Registration: personal key visited' 
+      FROM tj
+      WHERE tj.t1_name = 'User Registration: phone setup visited'
+      AND tj.t2_name = 'User Registration: personal key visited'
     UNION
-      SELECT 
-        'agency handoff' AS exp_name, 
-        duration, 
+      SELECT
+        'agency handoff' AS exp_name,
+        duration,
         tj.time as time
-      FROM tj 
-      WHERE tj.t1_name = 'User registration: agency handoff visited' 
-      AND tj.t2_name = 'User registration: agency handoff complete' 
+      FROM tj
+      WHERE tj.t1_name = 'User registration: agency handoff visited'
+      AND tj.t2_name = 'User registration: agency handoff complete'
   ) q
 );
 
 CREATE VIEW experience_durations_visitor_id AS (
   WITH tj as (
-    SELECT 
-      t1.name as t1_name, 
-      t2.name as t2_name, 
-      t1.user_id as t1_user_id, 
-      t1.visitor_id as t1_visitor_id, 
+    SELECT
+      t1.name as t1_name,
+      t2.name as t2_name,
+      t1.user_id as t1_user_id,
+      t1.visitor_id as t1_visitor_id,
       EXTRACT(epoch FROM (t2.time - t1.time)) AS duration,
-      t1.time as time 
-    FROM events t1 
-    JOIN events t2 
-    ON 
-      t1.user_id = t2.user_id 
-    AND 
+      t1.time as time
+    FROM events t1
+    JOIN events t2
+    ON
+      t1.user_id = t2.user_id
+    AND
       t1.visitor_id = t2.visitor_id
     WHERE EXTRACT(epoch FROM (t2.time - t1.time)) > 0
   )
   SELECT q.exp_name,q.duration,q.time FROM
   (
-      SELECT 
-        'email submission' AS exp_name, 
-        duration, 
+      SELECT
+        'email submission' AS exp_name,
+        duration,
         tj.time as time
-      FROM tj 
-      WHERE tj.t1_name = 'User Registration: enter email visited' 
-      AND tj.t2_name = 'User Registration: Email Submitted' 
+      FROM tj
+      WHERE tj.t1_name = 'User Registration: enter email visited'
+      AND tj.t2_name = 'User Registration: Email Submitted'
     UNION
-      SELECT 
-        'phone setup' AS exp_name, 
-        duration, 
+      SELECT
+        'phone setup' AS exp_name,
+        duration,
         tj.time as time
-      FROM tj 
-      WHERE tj.t1_name = 'User Registration: phone setup visited' 
-      AND tj.t2_name = 'User Registration: personal key visited' 
+      FROM tj
+      WHERE tj.t1_name = 'User Registration: phone setup visited'
+      AND tj.t2_name = 'User Registration: personal key visited'
     UNION
-      SELECT 
-        'agency handoff' AS exp_name, 
-        duration, 
+      SELECT
+        'agency handoff' AS exp_name,
+        duration,
         tj.time as time
-      FROM tj 
-      WHERE tj.t1_name = 'User registration: agency handoff visited' 
-      AND tj.t2_name = 'User registration: agency handoff complete' 
+      FROM tj
+      WHERE tj.t1_name = 'User registration: agency handoff visited'
+      AND tj.t2_name = 'User registration: agency handoff complete'
   ) q
 );
 
@@ -110,15 +110,14 @@ CREATE VIEW daily_active_users AS
 (
   SELECT count(DISTINCT events.user_id) AS count, events."time"::date AS "time"
   FROM events
-  WHERE events."time"::date >= (NOW() at time zone 'UTC' - interval '30' day) 
+  WHERE events."time"::date >= (NOW() at time zone 'UTC' - interval '30' day)
   GROUP BY events."time"::date
 );
 
 CREATE VIEW hourly_active_users AS
 (
-  SELECT count(DISTINCT events.user_id) AS count, events."time"::date AS "day", date_part('hour'::text, events."time") AS hr
+  SELECT count(DISTINCT events.user_id) AS count, events."time"::date AS "day", date_part('hour', events."time")::text AS hr
   FROM events
-  WHERE events."time"::date >= (NOW() at time zone 'UTC' - interval '30' day) 
-  GROUP BY events."time"::date, date_part('hour'::text, events."time")
+  WHERE events."time"::date >= (NOW() at time zone 'UTC' - interval '30' day)
+  GROUP BY events."time"::date, date_part('hour', events."time")::text
 );
-
