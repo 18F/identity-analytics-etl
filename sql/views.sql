327073a7b273147cee263316abc216fe7c879ff0
Colin Craig <colin.craig@gsa.gov>
2017-12-07 14:36:49 -0500

    Not sure how these views got left out

--- sql/views.sql
+++ sql/views.sql
@@ -9,6 +9,24 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE VIEW password_success_rate
+as (
+      SELECT SUM(success::integer)/COUNT(*)::float as success_rate,
+      date_trunc('hour', time) as hour
+      FROM events
+      WHERE name = 'Email and Password Authentication'
+      GROUP BY hour
+    );
+
+CREATE VIEW mfa_success_rate
+as (
+      SELECT SUM(success::integer)/COUNT(*)::float as success_rate,
+      date_trunc('hour', time) as hour
+      FROM events
+      WHERE name = 'Multi-Factor Authentication: max attempts reached'
+      GROUP BY hour
+    );
+
 CREATE VIEW experience_durations AS (
   WITH tj as (
     SELECT
