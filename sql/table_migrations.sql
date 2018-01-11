66fb00dfaf5b8df13b551d2b50c20fa5dab376d5
Colin Craig <colin.craig@gsa.gov>
2018-01-11 14:26:33 -0500

    Limit Experience duration view to one month. (#51)
    * Limit Experience duration view to one month.
    
    * Redshift compatible date add
    
    * Redshift compatible date add

--- sql/table_migrations.sql
+++ sql/table_migrations.sql
@@ -49,7 +49,9 @@ CREATE VIEW experience_durations AS (
       t1.user_id = t2.user_id
     AND
       t1.visit_id = t2.visit_id
-    WHERE EXTRACT(epoch FROM (t2.time - t1.time)) > 0
+    WHERE EXTRACT(epoch FROM (t2.time - t1.time)) > 0  
+    AND t1."time"::date >= (DATEADD(month, -1, GETDATE()) at time zone 'UTC') 
+    AND t2."time"::date >= (DATEADD(month, -1, GETDATE()) at time zone 'UTC') 
   )
   SELECT q.exp_name,q.duration,q.time FROM
   (
@@ -95,6 +97,8 @@ CREATE VIEW experience_durations_visitor_id AS (
     AND
       t1.visitor_id = t2.visitor_id
     WHERE EXTRACT(epoch FROM (t2.time - t1.time)) > 0
+    AND t1.time::date >= (DATEADD(month, -1, GETDATE()) at time zone 'UTC')  
+    AND t2.time::date >= (DATEADD(month, -1, GETDATE()) at time zone 'UTC') 
   )
   SELECT q.exp_name,q.duration,q.time FROM
   (
