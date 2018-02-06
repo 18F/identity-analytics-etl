8920253c970b76e2e8db2276d23055ed6ba00b52
Colin Craig <colin.craig@gsa.gov>
2018-02-05 20:04:56 -0500

    Use pip3 to build reqs

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cd build-analytics \
 && chmod u=rwx,go=r function.py \
 && mkdir lambda_$1_deploy \
 && cp -R src lambda_$1_deploy && cp function.py lambda_$1_deploy \
-&& pip install -Ur requirements.txt -t ./lambda_$1_deploy \
+&& pip3 install -Ur requirements.txt -t ./lambda_$1_deploy \
 && git clone https://github.com/jkehler/awslambda-psycopg2.git \
 && rm -rf lambda_$1_deploy/psycopg2 \
 && mv awslambda-psycopg2/psycopg2-3.6 lambda_$1_deploy/psycopg2 \
