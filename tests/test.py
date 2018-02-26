0a18b97244075a1264d968f0a2e90b5864e1035a
Jean Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-02-26 14:45:50 -0500

    revert test uploader comments, isolate database_test_cases instead

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,9 @@
 import unittest
 
-from test_database_connection import DataBaseTestCases
+# from test_database_connection import DataBaseTestCases
 from test_event_parser import EventParserTestCases
 from test_pageview_parser import PageViewParserTestCases
-#from test_uploader import UploaderTestCases
+from test_uploader import UploaderTestCases
 
 if __name__ == '__main__':
     unittest.main()
