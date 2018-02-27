18610597513b302ea725be6adafec3da9a3c21f8
Jean Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-02-27 13:48:35 -0500

    install psql on circleci container

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-# from test_database_connection import DataBaseTestCases
+from test_database_connection import DataBaseTestCases
 from test_event_parser import EventParserTestCases
 from test_pageview_parser import PageViewParserTestCases
 from test_uploader import UploaderTestCases
