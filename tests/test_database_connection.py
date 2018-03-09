5111491d5046c24a140881a784b82ef19a2fbbb7
Jean Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-03-09 10:38:32 -0500

    print more stuff

--- tests/test_database_connection.py
+++ tests/test_database_connection.py
@@ -21,7 +21,7 @@ class DataBaseTestCases(unittest.TestCase):
     def csv_upload(self):
         db_conn = self.setup()
         s = subprocess.Popen(["ls", "-R", "/home/circleci/identity-analytics-etl/tests/fixtures/"], stdout=subprocess.PIPE)
-        s.communicate()[0]
+        print(s.communicate()[0])
         path_to_csv = os.path.abspath(os.path.dirname(__file__))
         with open(os.path.join(path_to_csv,"fixtures/test_csv.csv"), 'w') as out:
             writer = csv.writer(out, delimiter=',')
