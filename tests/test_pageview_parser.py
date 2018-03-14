efcf64d3f85fc728a9658086947d25c50671dd2e
Jean-Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-03-14 16:11:41 -0400

    Use StringIO and BytesIO for Test Fixtures (#66)
    * import libs
    
    * use stringio instead of filesytem for test fixtures
    
    * refactor test page view parser to use io instead of filesystem
    
    * add a working circleci config
    
    * run everything, except database connection
    
    * fix typo
    
    * also remove test uploader
    
    * remove unwanted .vscode/settings.json file
    
    * re-write test uploader to use StringIO and add it to CI
    
    * remove unused .read() method
    
    * add io objects as dict values directly, instead of passing them by reference
    
    * add new line to config.yml
    
    * make TEST_EVENT_LOG_TXT and TEST_PAGEVIEW_LOG_TXT instance variables

--- tests/test_pageview_parser.py
+++ tests/test_pageview_parser.py
@@ -4,12 +4,24 @@ import os
 import json
 
 from context import PageViewParser
+from io import BytesIO
 
 
 class PageViewParserTestCases(unittest.TestCase):
     pageview_json = '{"method":"GET","path":"/?issuer=&timeout=true","format":"html","controller":"Users::SessionsController","action":"new","status":302,"duration":4.84,"location":"https://idp.staging.login.gov/?issuer=","user_id":"anonymous-uuid","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","ip":"24.124.56.64","host":"idp.staging.login.gov","timestamp":"2017-04-10 17:45:22 +0000","uuid":"58d753fe-4542-437f-a812-1f0f146cb4ec"}'
     pageview_json_no_id = '{"method":"GET","path":"/?issuer=&timeout=true","format":"html","controller":"Users::SessionsController","action":"new","status":302,"duration":4.84,"location":"https://idp.staging.login.gov/?issuer=","user_id":"anonymous-uuid","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","ip":"24.124.56.64","host":"idp.staging.login.gov","timestamp":"2017-04-10 17:45:22 +0000"}'
-    in_io = open("{}/fixtures/test_pageview_log.txt".format(os.path.dirname(os.path.realpath(__file__))), 'rb').read()
+    test_pageview_log_txt = BytesIO(b"""
+     2017-04-10T17:45:22.600Z idp 172.16.33.245 - - [10/Apr/2017:17:45:21 +0000] "GET / HTTP/1.1" 401 188 "-" "ELB-HealthChecker/2.0"
+     2017-04-10T17:45:23.600Z idp 172.16.33.245 - 18f [10/Apr/2017:17:45:22 +0000] "GET /?issuer=&timeout=true HTTP/1.1" 302 115 "https://idp.staging.login.gov/?issuer=" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+     2017-04-10T17:45:23.600Z idp 172.16.33.245 - 18f [10/Apr/2017:17:45:22 +0000] "GET /?issuer= HTTP/1.1" 200 9087 "https://idp.staging.login.gov/?issuer=" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+     2017-04-10T17:45:25.438Z idp {"method":"GET","path":"/?issuer=&timeout=true","format":"html","controller":"Users::SessionsController","action":"new","status":302,"duration":4.84,"location":"https://idp.staging.login.gov/?issuer=","user_id":"anonymous-uuid","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","ip":"24.124.56.64","host":"idp.staging.login.gov","timestamp":"2017-04-10 17:45:22 +0000","uuid":"58d753fe-4542-437f-a812-1f0f146cb4ec"}
+     2017-04-10T17:45:25.438Z idp {"method":"GET","path":"/?issuer=","format":"html","controller":"Users::SessionsController","action":"new","status":200,"duration":13.59,"user_id":"anonymous-uuid","user_agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36","ip":"24.124.56.64","host":"idp.staging.login.gov","timestamp":"2017-04-10 17:45:22 +0000","uuid":"a7617fe8-87b7-4f67-8e50-ebc90a2d3fbe"}
+     2017-03-31T19:38:48.835Z elk type=PATH msg=audit(1490935667.633:579960): item=0 name="/usr/src/linux-headers-3.13.0-107/include/dt-bindings/interrupt-controller/" inode=22764 dev=ca:01 mode=040755 ouid=0 ogid=0 rdev=00:00 nametype=PARENT
+     2017-03-31T19:38:48.835Z elk type=PATH msg=audit(1490935667.633:579960): item=1 name="/usr/src/linux-headers-3.13.0-107/include/dt-bindings/interrupt-controller/arm-gic.h" inode=23233 dev=ca:01 mode=0100644 ouid=0 ogid=0 rdev=00:00 nametype=DELETE
+     2017-04-06T06:57:09.449Z jenkins f++++++++++++++++: /var/chef/cache/identity-idp/spec/support/controller_helper.rb
+
+    """)
+    in_io = test_pageview_log_txt.read()
 
     def test_stream_csv(self):
         parser = PageViewParser()
