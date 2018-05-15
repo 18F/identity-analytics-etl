711479f083aef0f7f5c2ab375ee3cdf16161103f
Jean Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-05-15 13:53:21 -0400

    use correct parquet bucket name and access to dict values

--- function.py
+++ function.py
@@ -8,31 +8,31 @@ import src
 def set_redshift_configs():
     # The bucket name and filename
     # could all be defined inside of the lambda resource in terraform.
-    bucket = boto3.resource('s3').Bucket("login-gov-{}-{}-redshift-secrets".format(os.environ.get('env'), os.environ.get('acct_id')))
+    bucket = boto3.resource('s3').Bucket("login-gov-{}-{}-redshift-secrets".format(os.environ['env'], os.environ['acct_id']))
     data = yaml.load(bucket.Object('redshift_secrets.yml').get()['Body'])
     os.environ['REDSHIFT_URI'] = "redshift+psycopg2://awsuser:{redshift_password}@{redshift_host}/analytics".format(
         redshift_password=data['redshift_password'],
-        redshift_host=os.environ.get('redshift_host')
+        redshift_host=os.environ['redshift_host']
     )
 
 def lambda_handler(event, context):
     logging.basicConfig(level=logging.INFO)
     uploader_logger = logging.getLogger('uploader')
     set_redshift_configs()
-    trigger_file = event["Records"][0]["s3"]["object"]["key"]
-    dest_bucket = os.environ.get('dest_bucket')
-    source_bucket = os.environ.get('source_bucket')
-    bucket_parquet = os.environ.get('bucket_parquet')
-    hot_bucket = os.environ.get('hot_bucket')
+    trigger_file = event['Records'][0]['s3']['object']['key']
+    dest_bucket = os.environ['dest_bucket']
+    source_bucket = os.environ['source_bucket']
+    parquet_bucket = os.environ['parquet_bucket']
+    hot_bucket = os.environ['hot_bucket']
 
     uploader = src.Uploader(
         source_bucket,
         dest_bucket,
-        bucket_parquet,
+        parquet_bucket,
         hot_bucket,
         logger=uploader_logger,
         redshift=True,
-        encryption_key=os.environ.get('encryption_key'),
+        encryption_key=os.environ['encryption_key']
     )
 
     uploader.run(trigger_file=trigger_file)
