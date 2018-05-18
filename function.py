f676b26492883edcbd5d462dcdf4c2cff179a12d
Jean-Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-05-18 10:36:50 -0400

    Parameterize Environmental Variables in Lambda f(x)s (#83)
    * parameterize variables in hot function
    
    * parameterize environment variables in permanent lambda
    
    * use account_id instead of acct_id
    
    * make bucket names set in terraform
    
    * add acct_id to bucket name and function 2
    
    * add more parameters
    
    * parameterize variables in hot function
    
    * parameterize environment variables in permanent lambda
    
    * use account_id instead of acct_id
    
    * make bucket names set in terraform
    
    * make sure that invalid chars and oversized strings are loaded (#79)
    
    * make sure that invalid chars and oversized strings are loaded
    
    * remove truncatecolumns and add more info on acceptinvchars
    
    * Add Area Code and Country Code Logging (#82)
    
    * add skeleton classes and unit tests for area code parser
    
    * only focus on area code
    
    * amend context
    
    * add area_code test to CI
    
    *  add proper test context
    
    * fix keyerror in area code and remove undeed unit tests
    
    * unify country and area code into phone code
    
    * add country code test
    
    * add phone parser Query and Uploader
    
    * add phone parser logic and refine tests
    
    * add new line
    
    * add acct_id to bucket name and function 2
    
    * add more parameters
    
    * use correct encryption key id in uploader params
    
    * use ACTUAL correct Us-west-2 key id -_-
    
    * use correct parquet bucket name and access to dict values
    
    * use hot_bucket for function 2
    
    * use correct redshift host
    
    * it's acct_id not account_id
    
    * add events_phone to headers in hot f(x)
    
    * add region as env variable to lambda
    
    * set region env var only where needed

--- function.py
+++ function.py
@@ -8,31 +8,31 @@ import src
 def set_redshift_configs():
     # The bucket name and filename
     # could all be defined inside of the lambda resource in terraform.
-    bucket = boto3.resource('s3').Bucket("login-gov-{}-redshift-secrets".format(os.environ.get('env')))
+    bucket = boto3.resource('s3').Bucket("login-gov-{}-{}-redshift-secrets".format(os.environ['env'], os.environ['acct_id']))
     data = yaml.load(bucket.Object('redshift_secrets.yml').get()['Body'])
     os.environ['REDSHIFT_URI'] = "redshift+psycopg2://awsuser:{redshift_password}@{redshift_host}/analytics".format(
         redshift_password=data['redshift_password'],
-        redshift_host=os.environ.get('redshift_host')
+        redshift_host=os.environ['redshift_host']
     )
 
 def lambda_handler(event, context):
     logging.basicConfig(level=logging.INFO)
     uploader_logger = logging.getLogger('uploader')
     set_redshift_configs()
-    trigger_file = event["Records"][0]["s3"]["object"]["key"]
-    dest_bucket = "login-gov-{}-analytics".format(os.environ.get('env'))
-    source_bucket = "login-gov-{}-logs".format(os.environ.get('env'))
-    bucket_parquet = "login-gov-{}-analytics-parquet".format(os.environ['env'])
-    hot_bucket = "login-gov-{}-analytics-hot".format(os.environ['env'])
+    trigger_file = event['Records'][0]['s3']['object']['key']
+    dest_bucket = os.environ['dest_bucket']
+    source_bucket = os.environ['source_bucket']
+    parquet_bucket = os.environ['parquet_bucket']
+    hot_bucket = os.environ['hot_bucket']
 
     uploader = src.Uploader(
         source_bucket,
         dest_bucket,
-        bucket_parquet,
+        parquet_bucket,
         hot_bucket,
         logger=uploader_logger,
         redshift=True,
-        encryption_key=os.environ.get('encryption_key'),
+        encryption_key=os.environ['encryption_key']
     )
 
     uploader.run(trigger_file=trigger_file)
