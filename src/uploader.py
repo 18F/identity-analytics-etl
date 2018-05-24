a9ccaf48df1c57a484b3a898b99f2b6b4e0cc44a
Jean-Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-05-24 11:26:55 -0400

    update default KMS key id (#85)

--- src/uploader.py
+++ src/uploader.py
@@ -14,7 +14,7 @@ from .s3 import S3
 
 class Uploader:
 
-    def __init__(self, source_bucket, dest_bucket, dest_bucket_parquet, hot_bucket, logger=None, s3=None, parsers=None, redshift=False, encryption_key="5d2d42ec-6b7f-4705-a1bb-7f4021a0df4d", lookback_period=None):
+    def __init__(self, source_bucket, dest_bucket, dest_bucket_parquet, hot_bucket, logger=None, s3=None, parsers=None, redshift=False, encryption_key="b10a84ce-1f80-44bc-8d0f-7a547b45ce53", lookback_period=None):
         self.redshift = redshift
         self.source_bucket = source_bucket
         self.dest_bucket = dest_bucket
