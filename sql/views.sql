6bf12a709ec4e460ce80bc4b178dde9eef4e58f1
Colin Craig <colin.craig@gsa.gov>
2018-06-28 17:27:34 -0400

    wip

--- sql/views.sql
+++ sql/views.sql
@@ -29,12 +29,12 @@ as (
 
 CREATE VIEW mfa_success_rate
 as (
-      SELECT SUM(success::integer)/COUNT(*)::float as success_rate,
-      date_trunc('hour', time) as hour
-      FROM events
-      WHERE name = 'Multi-Factor Authentication: max attempts reached'
-      GROUP BY hour
-    );
+  SELECT SUM(success::integer)/COUNT(*)::float as success_rate,
+  date_trunc('hour', time) as hour
+  FROM events
+  WHERE name = 'Multi-Factor Authentication: max attempts reached'
+  GROUP BY hour
+);
 
 CREATE VIEW experience_durations AS (
   WITH tj as (
