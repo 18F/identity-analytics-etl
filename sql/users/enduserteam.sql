53da7903872413dc325e42a32bf09a159b727c67
Jean-Paul Mugizi <mugizico@users.noreply.github.com>
2018-09-13 17:35:15 -0400

    add @konklone on redshift (#131)

--- sql/users/enduserteam.sql
+++ sql/users/enduserteam.sql
@@ -1,5 +1,7 @@
 CREATE GROUP enduserteam;
-CREATE USER monfresh WITH password '$password';
+CREATE USER monfresh WITH password '$password'; # change $password to specific
 ALTER GROUP enduserteam ADD USER monfresh;
 GRANT USAGE ON SCHEMA "public" TO GROUP enduserteam;
 GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO GROUP enduserteam;
+CREATE USER ericmill WITH password '$my_password'; 
+ALTER GROUP enduserteam ADD USER ericmill;
