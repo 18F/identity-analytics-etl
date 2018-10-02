198b768cb5ee54fc11e11ca8b1da7c33b0b06c7a
Moncef Belyamani <moncef.belyamani@gsa.gov>
2018-10-02 17:43:41 -0400

    Create a Redshift user for Sanhe Hu

--- sql/users/enduserteam.sql
+++ sql/users/enduserteam.sql
@@ -3,5 +3,7 @@ CREATE USER monfresh WITH password '$password'; # change $password to specific
 ALTER GROUP enduserteam ADD USER monfresh;
 GRANT USAGE ON SCHEMA "public" TO GROUP enduserteam;
 GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO GROUP enduserteam;
-CREATE USER ericmill WITH password '$my_password'; 
+CREATE USER ericmill WITH password '$my_password';
 ALTER GROUP enduserteam ADD USER ericmill;
+CREATE USER sanhehu WITH password '$my_password';
+ALTER GROUP enduserteam ADD USER sanhehu;
