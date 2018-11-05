f875ff092b37e07bc1b2d3d938d7c73802335813
Brian Crissup <crissupb@amazon.com>
2018-11-05 11:04:54 -0500

    Fix typo browser_device_type (#145)

--- sql/create_table/events_devices.sql
+++ sql/create_table/events_devices.sql
@@ -8,7 +8,7 @@ create table if not exists public.events_devices (
     browser_platform_name varchar(255),
     browser_platform_version varchar(255),
     browser_device_name varchar(255),
-    browser_device_typed varchar(255),
+    browser_device_type varchar(255),
     browser_bot boolean,
     time timestamp
 );
