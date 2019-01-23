b292da1c8736853333a87df2ecd9227296d931d5
Sanhe <MacHu-GWU@users.noreply.github.com>
2019-01-23 10:36:46 -0500

    Add queries for important metrics (#163)
    * add several user count query
    
    * add monthly MFA signup count query
    
    * fix test requirement
    
    * fix typo
    
    * Correct at least two sp query

---
+++ docs/source/02-Analytics-Query/User-Count/04-At-Least-One-SP.sql
@@ -0,0 +1,13 @@
+/*
+**Definition**: number of Users who created their Login.gov account and completed registering MFA as a result of initiating their Login.gov session through at least one SP.
+
+**SQL Description**:
+
+unlike ``users`` table, any users in ``identities`` have been finished MFA setup.
+*/
+
+\set end_time '''2019-01-01'''
+SELECT
+    COUNT(DISTINCT(identities.user_id)) AS at_least_1sp_user_counts
+FROM identities
+WHERE identities.created_at < :end_time;
