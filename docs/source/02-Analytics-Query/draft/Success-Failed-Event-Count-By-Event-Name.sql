ea9a38b8cef9ce3571a96342eff841c1c75817d2
Sanhe Hu <husanhe@gmail.com>
2019-03-07 12:50:37 -0500

    only counts event from service provider

--- docs/source/02-Analytics-Query/draft/Success-Failed-Event-Count-By-Event-Name.sql
+++ docs/source/02-Analytics-Query/draft/Success-Failed-Event-Count-By-Event-Name.sql
@@ -3,7 +3,7 @@
 
 We want to compare data quality in redshift to its source Cloudwatch log.
 
-How many events we have for each event name can tell how many events are missing in Redshift.
+How many events we have for each event name can tell how many events are missing in Redshift. **Only counts traffic from service providers**.
 
 Since we use event name heavily, **it helps us making decision on whether we should use Redshift for specific question**.
 
@@ -27,6 +27,8 @@ FROM (
     FROM events
     WHERE
         events.time BETWEEN :start_time AND :end_time
+        AND events.service_provider IS NOT NULL
+        AND events.service_provider != ''
     GROUP BY name, success
     ORDER BY name ASC, success DESC
 ) T
